24a26ba40d75dba4dd34296e6ee454af478cfbf6
Hamilton Mercado Cuellar <hamelshmc@gmail.com>
2023-07-25 20:26:10 +0200

    ⭐️ feat: Cabecera

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { loadingStore } from '@/stores/loadingStore'
+
+const store = loadingStore()
+</script>
 
 <template>
+  <header class="flex items-center justify-between p-4 shadow-md h-14">
+    <router-link v-slot="{ navigate }" to="/">
+      <h1 class="text-lg lg:text-2xl font-bold tracking-widest text-blue-400" @click="navigate">
+        Podcaster
+      </h1>
+    </router-link>
+    <div v-if="store.isLoading" class="loader ease-linear"></div>
+  </header>
   <RouterView></RouterView>
 </template>
 
