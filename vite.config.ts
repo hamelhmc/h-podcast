bed2a5ee8cd94e079f675062bda0880184df486e
Hamilton Mercado Cuellar <hamelshmc@gmail.com>
2023-07-25 20:45:15 +0200

    ⭐️ feat: base repo name

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite'
 import checker from 'vite-plugin-checker'
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/h-podcast/',
   plugins: [
     vue(),
     checker({
